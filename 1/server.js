6a2fa2679b89cb93760e61bd249fd64751d48b5b
Texran <texranhamidzada@gmail.com>
2022-12-24 05:40:25 +0400

    vfd

--- 1/server.js
+++ 1/server.js
@@ -1,14 +1,40 @@
+cards = [
+  {
+    id: 0,
+    number: "2323456789",
+  },
+  {
+    id: 1,
+    number: "9953456789",
+  },
+  {
+    id: 2,
+    number: "2964456789",
+  },
+  {
+    id: 3,
+    number: "1123456789",
+  },
+  {
+    id: 4,
+    number: "4123456789",
+  },
+];
+
 const express = require("express");
 const app = express();
 
-app.get("/", function (req, res) {
-  const fs = require("fs");
-  fs.readFile("./data.json", "utf-8", (err, data) => {
-    if (!err) res.send(data);
-    else console.log(err);
-  });
+app.get("/card", function (req, res) {
+  res.send(cards);
+});
+
+app.get("/card/:id", function (req, res) {
+  const card = JSON.stringify(cards.find((item) => item.id == req.params.id));
+  if (!card) res.status(404).send();
+
+  res.status(200).json(card);
 });
 
 app.listen(3000, function () {
-  console.log("Texran on port 3000!");
+  console.log("Example app listening on port 3000!");
 });
